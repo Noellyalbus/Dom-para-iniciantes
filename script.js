095b9de9449e475c8d04d9583db001894e1f5f7b
Noelly Albus <115089251+Noellyalbus@users.noreply.github.com>
2025-02-13 12:33:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,93 @@
-// verifique a distância da primeira imagem
-// em relação ao topo da página
-const img = document.querySelector('img');
-const imgTop = img.offsetTop;
-
-console.log(imgTop);
-
-// Retorne a soma da largura de todas as imagens
-function somaImagens(){
-    const imagens = document.querySelectorAll('img');
-    let soma = 0;
-    imagens.forEach((imagem) => {
-        soma += imagem.offsetWidth;
-    });
-    console.log(soma);
-};
-
-
-window.onload = function(){
-    somaImagens();
-};
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
-links.forEach((links) => {
-    const linkWidth = links.offsetWidth;
-    const linkHeight = links.offsetHeight;
-    if(linkWidth >= 48 && linkHeight >= 48){
-        console.log(linkHeight, 'Possui boa acessibilidade')
-    }else{
-        console.log(linkHeight, 'Não possui boa acessibilidade')
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo');
+    
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo')
+            });
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
+        });
     }
-});
+}
+initTabNav();
 
-console.log(links);
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    
+    if(accordionList.length){
+        accordionList[0].classList.add('ativo');
+        accordionList[0].nextElementSibling.classList.add('ativo');
+        
+        function activeAccordion(){
+            this.classList.toggle('ativo');
+            this.nextElementSibling.classList.toggle('ativo');
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        })
+    }
+}
+initAccordion();
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const browserSmall = window.matchMedia('(max-width: 720px)').matches;
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#]');
 
-if(browserSmall){
-    const menu = document.querySelector('.menu');
-    menu.classList.add('menu-mobile')
-};
+    function scrollSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    
+        // Forma alternativa
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+    }
+    
+    linksInternos.forEach((link) =>{
+        link.addEventListener('click',scrollSection);
+    });
+}
+initScrollSuave();
+
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.6;
+    
+        function animaScroll(){
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible)
+                    section.classList.add('ativo');
+                else
+                section.classList.remove('ativo');
+            })
+        }
+        animaScroll();
+        
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+initAnimacaoScroll();
+
